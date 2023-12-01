0b02d3c596f3b1c5919ff3a3502bfa1eec3666b6
Golem1988 <34597809+Golem1988@users.noreply.github.com>
2023-12-01 02:34:46 +0100

    Updated hero information functionality

--- Assets/Scripts/GUI/HeroInfoInterface.cs
+++ Assets/Scripts/GUI/HeroInfoInterface.cs
@@ -6,32 +6,158 @@ using UnityEngine.UIElements;
 
 public class HeroInfoInterface : MonoBehaviour
 {
-    public GameObject TestHero;
+    [SerializeField] GameObject HeroInfoPanelGameObject;
+    [SerializeField] GameObject TestHero;
 
     public TMP_Text heroListName;
     public TMP_Text heroSlots;
+
     public TMP_Text heroName;
     public TMP_Text heroLevel;
+
     public TMP_Text heroHP;
     public TMP_Text heroMP;
     public TMP_Text heroAtk;
     public TMP_Text heroMatk;
     public TMP_Text heroDef;
+
     public TMP_Text heroDodge;
     public TMP_Text heroHit;
     public TMP_Text heroCrit;
     public TMP_Text heroSpeed;
+
     public TMP_Text heroStr;
     public TMP_Text heroInt;
     public TMP_Text heroDex;
     public TMP_Text heroAgi;
     public TMP_Text heroSta;
-    public TMP_Text heroPts;
+
+    public TMP_Text heroStatpts;
+
     public TMP_Text heroLoyalty;
     public TMP_Text heroExp;
 
+    public int newStr;
+    public int newInt;
+    public int newDex;
+    public int newAgi;
+    public int newSta;
+
 
     private void Start()
+    {
+        UpdateStats();
+    }
+
+
+    public void IncrStr()
+    {
+        if (TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints > 0)
+        {
+            TestHero.GetComponent<HeroStateMachine>().hero.strength++;
+            TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints--;
+            UpdateStats();
+        }
+        else
+        {
+            Debug.Log("You don't have enough statpoints to destribute!");
+        }
+    }
+
+    public void DecrStr()
+    {
+        TestHero.GetComponent<HeroStateMachine>().hero.strength--;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints++;
+        UpdateStats();
+    }
+
+
+    public void IncrInt()
+    {
+        if (TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints > 0)
+        {
+            TestHero.GetComponent<HeroStateMachine>().hero.intellect++;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints--;
+        UpdateStats();
+        }
+            else
+            {
+                Debug.Log("You don't have enough statpoints to destribute!");
+            }
+    }
+
+    public void DecrInt()
+    {
+        TestHero.GetComponent<HeroStateMachine>().hero.intellect--;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints++;
+        UpdateStats();
+    }
+
+
+    public void IncrDex()
+    {
+        if (TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints > 0)
+        {
+            TestHero.GetComponent<HeroStateMachine>().hero.dexterity++;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints--;
+        UpdateStats();
+        }
+        else
+        {
+                    Debug.Log("You don't have enough statpoints to destribute!");
+        }
+     }
+
+    public void DecrDex()
+    {
+        TestHero.GetComponent<HeroStateMachine>().hero.dexterity--;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints++;
+        UpdateStats();
+    }
+
+    public void IncrAgi()
+    {
+        if (TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints > 0)
+        {
+            TestHero.GetComponent<HeroStateMachine>().hero.agility++;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints--;
+        UpdateStats();
+        }
+        else
+            {
+                Debug.Log("You don't have enough statpoints to destribute!");
+            }
+        }
+
+    public void DecrAgi()
+    {
+        TestHero.GetComponent<HeroStateMachine>().hero.agility--;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints++;
+        UpdateStats();
+    }
+
+    public void IncrSta()
+    {
+        if(TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints > 0)
+        {
+            TestHero.GetComponent<HeroStateMachine>().hero.stamina++;
+            TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints--;
+            UpdateStats();
+        }
+        else
+        {
+            Debug.Log("You don't have enough statpoints to destribute!");
+        }
+    }
+
+    public void DecrSta()
+    {
+        TestHero.GetComponent<HeroStateMachine>().hero.stamina--;
+        TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints++;
+        UpdateStats();
+    }
+
+    public void UpdateStats()
     {
         heroListName.text = TestHero.GetComponent<HeroStateMachine>().hero.theName;
 
@@ -56,47 +182,8 @@ public class HeroInfoInterface : MonoBehaviour
         heroAgi.text = TestHero.GetComponent<HeroStateMachine>().hero.agility.ToString();
         heroSta.text = TestHero.GetComponent<HeroStateMachine>().hero.stamina.ToString();
 
-        heroPts.text = TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints.ToString();
+        heroStatpts.text = TestHero.GetComponent<HeroStateMachine>().hero.unspentStatPoints.ToString();
 
         heroExp.text = TestHero.GetComponent<HeroStateMachine>().hero.level.experience.ToString() + " / " + TestHero.GetComponent<HeroStateMachine>().hero.level.requiredExp.ToString();
-
     }
-
-    void IncrStr()
-    {
-        TestHero.GetComponent<HeroStateMachine>().hero.strength++;
-    }
-
-    void IncrInt()
-    {
-        TestHero.GetComponent<HeroStateMachine>().hero.intellect++;
-    }
-
-    void IncrDex()
-    {
-        TestHero.GetComponent<HeroStateMachine>().hero.dexterity++;
-    }
-
-    void IncrAgi()
-    {
-        TestHero.GetComponent<HeroStateMachine>().hero.agility++;
-    }
-
-    void IncrSta()
-    {
-        TestHero.GetComponent<HeroStateMachine>().hero.stamina++;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
